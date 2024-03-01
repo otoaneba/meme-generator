f5e9df12bda70e60432ced7fc7eddc4610e77884
Naoto Abe <naotoabe1@gmail.com>
2024-03-01 10:51:21 +0100

    fix syntax error

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,11 @@ module.exports = {
             "presets": [
               "babel-preset-env",
               "babel-preset-react"
-            ]
-          }
+            ],
+            "plugins": [
+              "babel-plugin-transform-object-rest-spread",
+            ],
+          },
         },
         "exclude": /node_modules/,
         "test": /\.js$/
